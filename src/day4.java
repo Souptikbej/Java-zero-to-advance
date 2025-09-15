83a6697f29201aceb81d3aa9178acf75fb4c8dc9
Souptikbej <bejsouptik@gmail.com>
2025-09-15 22:36:34 +0530

    day 4

--- src/day4.java
+++ src/day4.java
@@ -39,9 +39,42 @@ public class day4 {
             System.out.print(i+ " ");
         }
 
-        System.out.println("2. Print first 10 multiples of 7 using a loop.");
+
+        System.out.println("\n2. Print first 10 multiples of 7 using a loop.");
+        for (int i = 1; i<=10 ; i++){
+            System.out.println("7 X "+i+" = "+7*i);
+        }
 
 
+        System.out.println("3. Print a multiplication table (up to 10x10).");
+        for (int i= 1 ; i<=10 ; i++){
+            for (int j=1 ; j<=10 ; j++){
+                System.out.println(i+" X "+j+" = "+i*j);
+            }
+        }
+
+        System.out.println("4. Print a hollow square of stars. Example for n=5:");
+        int n=5;
+        for (int i= 1 ; i<= n ; i++){
+            for (int j=1 ; j<=n ; j++){
+                if (i==1 || i==n || j==1 || j==n){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("5. Print Floyd’s Triangle (numbers in triangle form):");
+        int k=1;
+        for (int i =1 ; i<= 4 ; i++){
+            for (int j= 1 ; j<=i ; j++){
+                System.out.print(k++ +" ");
+            }
+            System.out.println();
+        }
 
     }
 }
