532549baf2b3d1c2a08378ba5d926c96c759a677
Souptikbej <bejsouptik@gmail.com>
2025-10-04 23:55:53 +0530

    day 6

--- src/day6.java
+++ src/day6.java
@@ -3,50 +3,67 @@ import java.util.Scanner;
 public class day6 {
     public static void main(String[] args) {
         Scanner obj=new Scanner(System.in);
-        System.out.print("Choose which one :\n" +
-                "1.Method to check if a number is prime\n"+
-                "2.Method to find factorial of a number\n"+
-                "3.Method to find GCD of two numbers\n"+
-                "4.Method to check if a string is palindrome\n"+
-                "5.Method to calculate Fibonacci series up to n terms\n"
-        );
-        int n= obj.nextInt();
-        switch (n){
-            case 1:
-                System.out.print("Enter number :");
-                int num3= obj.nextInt();
-                prime();
-                System.out.println();
-                break;
-            case 2:
-                factorial();
-                System.out.println();
-                break;
-            case 3:
-                System.out.print("Enter First number :");
-                int num1= obj.nextInt();
-                System.out.print("Enter Second number :");
-                int num2= obj.nextInt();
-                System.out.println("The Greatest common divisor (GCF) of two numbers are: "+ GCD(num1,num2));
-                break;
-            case 4:
-                palin();
-                System.out.println();
-                break;
-            case 5:
-                fibo();
-                System.out.println();
-                break;
-
+        while(true){
+            System.out.print("\nChoose which one :\n" +
+                    "1.Method to check if a number is prime\n"+
+                    "2.Method to find factorial of a number\n"+
+                    "3.Method to find GCD of two numbers\n"+
+                    "4.Method to check if a string is palindrome\n"+
+                    "5.Method to calculate Fibonacci series up to n terms\n"+
+                    "6.Exit\n"
+            );
+            int n= obj.nextInt();
+            switch (n){
+                case 1:
+                    System.out.print("Enter number :");
+                    int num3= obj.nextInt();
+                    System.out.println("It is Prime number :"+prime(num3));
+                    break;
+                case 2:
+                    System.out.print("Enter number :");
+                    int num4= obj.nextInt();
+                    System.out.println("Factorial of this number is :"+factorial(num4));
+                    break;
+                case 3:
+                    System.out.print("Enter First number :");
+                    int num1= obj.nextInt();
+                    System.out.print("Enter Second number :");
+                    int num2= obj.nextInt();
+                    System.out.println("The Greatest common divisor (GCF) of two numbers are: "+ GCD(num1,num2));
+                    break;
+                case 4:
+                    System.out.print("Enter the string :");
+                    obj.nextLine(); // consume newline
+                    String str = obj.nextLine();
+                    System.out.println("This string is palindrome : "+palin(str));
+                    break;
+                case 5:
+                    System.out.print("Enter the required steps :");
+                    int num5= obj.nextInt();
+                    fibo(num5);
+                    break;
+                case 6:
+                    System.out.println("System exiting.....");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Choose right index number!");
+            }
         }
     }
-    public static int prime(){
-
-        return 0;
+    public static boolean prime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
     }
-    public static int factorial(){
-
-        return 0;
+    public static int factorial(int num4){
+        int fact=1;
+        for (int i=1;i<=num4;i++){
+            fact*=i;
+        }
+        return fact;
     }
     public static int GCD(int num1,int num2){
         while (num2 != 0) {
@@ -56,11 +73,21 @@ public class day6 {
         }
         return num1;
     }
-    public static void palin(){
-
-
+    public static boolean palin(String str){
+        String str1="";
+        int l= str.length();
+        for (int i=l-1;i>=0;i--){
+            str1=str1+str.charAt(i);
+        }
+        return str1.equals(str);
     }
-    public static void fibo(){
-
+    public static void fibo(int num5){
+        int f1=0,f2=1,f3;
+        for (int i=1;i<=num5;i++){
+            System.out.print(f1+" ");
+            f3=f1+f2;
+            f1=f2;
+            f2=f3;
+        }
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-            System.out.println("i = " + i*2);
+        Scanner obj=new Scanner(System.in);
+        System.out.print("Enter the string :");
+        String str= obj.nextLine();
+        String str1="";
+        int l= str.length();
+        for (int i=l-1;i>=0;i--){
+            str1=str1+str.charAt(i);
         }
+        if (str1.equals(str))
+            System.out.println("yes");
+        else
+            System.out.println("no");
     }
 }
